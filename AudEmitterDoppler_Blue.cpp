38fe76efbc278fbbaef5e46c2472ca8b931ce949
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2018-06-08 15:10:00 +0000

    Exposing the AudEmitterDoppler so it can take no arguments.
    https://jira.ccpgames.com/browse/EVEDEV-5226
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/audio2/": change = 1328668]

--- AudEmitterDoppler_Blue.cpp
+++ AudEmitterDoppler_Blue.cpp
@@ -21,7 +21,7 @@ const Be::ClassInfo* AudEmitterDoppler::ExposeToBlue()
 	EXPOSURE_BEGIN( AudEmitterDoppler, "RAII wrapper for Wwise gameobjects. Python constructor takes in a name of the object as a string" )
 		MAP_INTERFACE( AudEmitter )
 
-		MAP_METHOD_AND_WRAP( "__init__", Py__init__, 
+		MAP_METHOD_AND_WRAP_OPTIONAL_ARGS( "__init__", Py__init__, 3,
 							"Description:\n"	
 							 "\tCreates a new Doppler audio emitter.\n"
 							 "\tReturns, void\n"
