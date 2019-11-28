36b83569306d317a7ff752ec2fba2295451bbc1a
CCP Zoetrope <eric@ccpgames.com>
2019-11-28 15:07:56 +0000

    [eve MAINLINE][audio] * Fix problem where sound emitter positioning is causing a sof hull scene to reload over and over. * Fix defect where trying to debug audio emitters when audio was not enabled was causing a crash. * Make some audio code slightly more readable.
    https://jira.ccpgames.com/browse/TC-249
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/audio2/": change = 1621458]

--- src/AudEmitter.cpp
+++ src/AudEmitter.cpp
@@ -40,29 +40,32 @@ void AudEmitter::GetDebugOptions( Tr2DebugRendererOptions& options )
 
 void AudEmitter::RenderDebugInfo( ITr2DebugRenderer2& renderer )
 {
-	if ( !g_debugDisplayAllEmitters )
+	if ( g_audioInitialized )
 	{
-		if ( !renderer.HasOption( GetRawRoot(), "AudioEmitters" ) )
+		if ( !g_debugDisplayAllEmitters )
 		{
-			return;
+			if ( !renderer.HasOption( GetRawRoot(), "AudioEmitters" ) )
+			{
+				return;
+			}
 		}
+
+		if ( !m_debugColor )
+		{
+			float minRange = 0.4f;
+			float maxRange = 0.9f;
+			float rand1 = static_cast<float>(minRange + (rand() / double( RAND_MAX )) * (maxRange - minRange));
+			float rand2 = static_cast<float>(minRange + (rand() / double( RAND_MAX )) * (maxRange - minRange));
+			float rand3 = static_cast<float>(minRange + (rand() / double( RAND_MAX )) * (maxRange - minRange));
+			m_debugColor = Color( rand1, rand2, rand3, 1.0f );
+		}
+
+		const float emitterRange = AK::SoundEngine::Query::GetMaxRadius( m_ID );
+
+		uint32_t debugSphereSegments = static_cast<uint32_t>(8.f + emitterRange / 5000.f);
+		debugSphereSegments = (debugSphereSegments < 25) ? debugSphereSegments : 25; // limit segment growth to 25
+
+		renderer.DrawSphere( this, m_debugPosition, emitterRange, debugSphereSegments, ITr2DebugRenderer2::Wireframe, Tr2DebugColor( m_debugColor ) );
+		renderer.DrawText( TRI_DBG_FONT_SMALL, m_debugPosition, m_debugColor, m_name.c_str() );
 	}
-	
-	if ( !m_debugColor )
-	{
-		float minRange = 0.4f;
-		float maxRange = 0.9f;
-		float rand1 = static_cast< float >( minRange + (rand() / double( RAND_MAX ) ) * (maxRange - minRange)); 
-		float rand2 = static_cast< float >( minRange + (rand() / double( RAND_MAX ) ) * (maxRange - minRange)); 
-		float rand3 = static_cast< float >( minRange + (rand() / double( RAND_MAX ) ) * (maxRange - minRange));
-		m_debugColor = Color( rand1, rand2, rand3, 1.0f);
-	}
-
-	const float emitterRange = AK::SoundEngine::Query::GetMaxRadius(m_ID);
-
-	uint32_t debugSphereSegments = static_cast< uint32_t >( 8.f + emitterRange / 5000.f );
-	debugSphereSegments = ( debugSphereSegments < 25 ) ? debugSphereSegments : 25; // limit segment growth to 25
-
-	renderer.DrawSphere( this, m_debugPosition, emitterRange, debugSphereSegments, ITr2DebugRenderer2::Wireframe, Tr2DebugColor(m_debugColor));
-	renderer.DrawText( TRI_DBG_FONT_SMALL, m_debugPosition, m_debugColor, m_name.c_str() );
 }
