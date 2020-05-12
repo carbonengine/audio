a384bc375e444461fce4d5c65941802eb8c5b8f7
Kristinn Þór Sigurbergsson <kristinn@ccpgames.com>
2020-05-12 15:47:59 +0000

    Merging using //eve/staging/2020-EMBLIC->//eve/development/MAINLINE
    Change 1718131 by hrafng@eve_branches_staging_2020-EMBLIC_hrafn-pc on 2020/05/04 12:39:52
    
    	Merging using //eve/sandbox/TEH-CHAPTER-THREE->//eve/staging/2020-EMBLIC up to CL 1718110
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/trinity/Audio/": change = 1724258]

--- include/ITr2AudEmitter.h
+++ include/ITr2AudEmitter.h
@@ -18,7 +18,7 @@ BLUE_INTERFACE( ITr2AudEmitter ) : public IRoot
 	virtual void Initialize( const std::string& name, const std::wstring& prefix, const Vector3& position ) = 0;
 	virtual int SetPosition( const Vector3& front, const Vector3& top, const Vector3& pos ) = 0;
 	virtual void SetName( const std::string& name ) = 0;
-	virtual unsigned int SendEvent( const std::wstring& name ) = 0;
+	virtual unsigned int SendEvent( const std::wstring& name, bool bypassPrefix = false ) = 0;
 	virtual void SetSwitch( const std::wstring& switchGroup, const std::wstring& switchState ) = 0;
 	virtual void SetRTPC( const std::wstring& rtpcName, float rtpcValue ) = 0;
 	virtual std::string GetName() = 0;
