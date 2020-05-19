3f45dd855d6e5d51717e8223e362d0e3e26bd801
Kristinn Þór Sigurbergsson <kristinn@ccpgames.com>
2020-05-19 08:57:20 +0000

    integrating to //eve/branches/development/MAINLINE/... using //eve/staging/2020-EMBLIC->//eve/development/MAINLINE
    85ce0796-3613-4d83-a4f3-bbdb8850393b
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/trinity/Audio/": change = 1729228]

--- include/ITr2AudEmitter.h
+++ include/ITr2AudEmitter.h
@@ -21,6 +21,7 @@ BLUE_INTERFACE( ITr2AudEmitter ) : public IRoot
 	virtual unsigned int SendEvent( const std::wstring& name, bool bypassPrefix = false ) = 0;
 	virtual void SetSwitch( const std::wstring& switchGroup, const std::wstring& switchState ) = 0;
 	virtual void SetRTPC( const std::wstring& rtpcName, float rtpcValue ) = 0;
+	virtual int SetAttenuationScalingFactor( const float scalingFactor ) = 0;
 	virtual std::string GetName() = 0;
 };
 
