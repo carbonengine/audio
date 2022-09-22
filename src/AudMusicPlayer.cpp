b9a31e02ebcccf694f5fd1cc6e4e95d678e6b39f
CCP Zoetrope <eric@ccpgames.com>
2022-09-22 16:47:57 +0000

    * Make audio emitters be culled by default when they're first spawned so we don't send crazy values to Wwise for a frame. * Make it so the music and UI audio emitters are placed at 0,0,0 because they were getting culled due to the fact that the initital position of audio emitters was changed to be super far away.
    https://jira.ccpgames.com/browse/EO-34534
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/carbon/src/audio2/": change = 2119503]

--- src/AudMusicPlayer.cpp
+++ src/AudMusicPlayer.cpp
@@ -5,6 +5,7 @@ AudMusicPlayer::AudMusicPlayer( IRoot* lockobj ) : AudEmitter( MUSIC_GAME_OBJ_ID
 {
 	m_name = "Music";
 	m_additionalCullingWeight = std::numeric_limits<float>::max();
+	m_position = Vector3( 0, 0, 0 );
 	RegisterWwiseObject();
 }
 
