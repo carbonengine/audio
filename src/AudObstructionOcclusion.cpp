d17910c9778c8ad4c0fd4741d9be63d7cc42fe07
phevosccp <phevos@ccpgames.com>
2026-07-21 10:58:43 +0000

    add empty AudObstructionOcclusion class implementation

---
+++ src/AudObstructionOcclusion.cpp
@@ -0,0 +1,10 @@
+////////////////////////////////////////////////////////////
+//
+// Creator: Phevos Rinis
+// Creation Date: Jul 2026
+// Copyright (c) 2026 CCP Games
+//
+
+#include "AudObstructionOcclusion.h"
+#include "AudManager.h"
+
---
+++ src/AudObstructionOcclusion.h
@@ -0,0 +1,11 @@
+////////////////////////////////////////////////////////////
+//
+// Creator: Phevos Rinis
+// Creation Date: Jul 2026
+// Copyright (c) 2026 CCP Games
+//
+
+#pragma once
+
+class AudObstructionOcclusion
+{}
